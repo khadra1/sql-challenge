951a15ab59efe1c5f90e67da2889e18d6205aecc
khadra <67019030+khadra1@users.noreply.github.com>
2022-05-09 11:38:04 +0100

    Update queries.sql

--- Employee-SQL/queries.sql
+++ Employee-SQL/queries.sql
@@ -34,8 +34,8 @@ AND last_name LIKE 'B%';
 -- employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e 
-INNER JOIN dept_emp de ON e.emp_no = de.emp_no
-INNER JOIN departments d ON de.dept_no = d.dept_no
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
 --List all employees in the Sales and Development departments, including their 
